b79cd56b799720f48fd56953abc6280d6fb06533
Gabriel Eduardo Soler da Angela <gabriel.angela@anchieta.br>
2022-11-09 13:00:33 -0300

    page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,73 @@
 import Image from "next/future/image";
+
+import { useKeenSlider } from "keen-slider/react";
+
 import { HomeContainer, Product } from "../styles/pages/home";
 
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
 
+import "keen-slider/keen-slider.min.css";
 
-export default function Home() {
+export default function Home(props) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={} />
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
       </Product>
     </HomeContainer>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  console.log("rodou");
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  };
+};
